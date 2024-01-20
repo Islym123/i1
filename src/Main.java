769a4d1818163adf5ed5aacab76c56bf4988620a
Islym <islamdulatov5@gmail.com>
2024-01-20 23:55:52 +0300

    испарвил ошибку

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-        System.out.print("Введите число>");
+        System.out.print("Введите число ограничитель цикла>");
+        int n = Input.nextInt();
+        System.out.print("Введите переменную>");
         double x = Input.nextDouble();
         if (x < 1 && x > -1)
         {
             double result = 0;
-            for (int i = 0; i < x; i++)
+            for (int i = 0; i < n; i++)
             {
                 result += Math.pow(x, i * 2 + 1) / 2 * i + 1;
             }
